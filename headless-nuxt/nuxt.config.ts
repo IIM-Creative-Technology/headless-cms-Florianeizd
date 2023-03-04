1ab16b18bec60a1b04d533638026516b975d6e9f
Florianeizd <floriane.izd45@gmail.com>
2023-03-04 17:20:30 +0100

    :100:
    Front Finish

--- headless-nuxt/nuxt.config.ts
+++ headless-nuxt/nuxt.config.ts
@@ -1,9 +1,33 @@
+import { link } from "fs";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     modules: ['@nuxtjs/strapi'],
+    
     strapi: {
         url: process.env.STRAPI_URL || 'http://localhost:1331',
         prefix: '/api',
         version: 'v4',
-      }
+    },
+    app: {
+      head: {
+        meta: [
+          // <meta name="viewport" content="width=device-width, initial-scale=1">
+      
+        ],
+        script: [
+          // <script src="https://myawesome-lib.js"></script>
+         
+        ],
+        link: [
+          // <link rel="stylesheet" href="https://myawesome-lib.css">
+          
+        ],
+    
+        
+      }, 
+    }
 })
+
+
+
--- headless-nuxt/pages/index.vue
+++ headless-nuxt/pages/index.vue
@@ -1,19 +1,44 @@
 <template>
-<div v-if="projects">
-    <h1>Hello World</h1>
-    <div v-if="types">
-        <button @click="filterSelection('all')">All</button>
-        <button v-for="projectType in types" @click="filterSelection(projectType)">
-            {{ projectType }}
-        </button>
-    </div>
-    <nuxt-link :to="`projects/${project.slug}`" v-for="project in filteredSelection">
-        {{ project.name}} <br>
-    </nuxt-link>
+<body>
+<ul>
+  <li><a class="active" href="">Home</a></li>
+  <li><a href="http://localhost:3000/projects/laravel">Laravel</a></li>
+  <li><a href="http://localhost:3000/projects/react">React</a></li>
+  <li><a href="http://localhost:3000/projects/todolist">Todolist</a></li>
+</ul>
+
+<div style="margin-left:25%;padding:1px 16px;height:1000px;">
+    <div v-if="projects">
+            <h1>Portfolio</h1>
+            <h2>Floriane IZARD</h2>
+            <div v-if="types">
+                <div><button class="button-56" @click="filterSelection('all')">All</button></div>
+                <br>
+                <div>
+                    <button class="button-56" v-for="projectType in types" @click="filterSelection(projectType)">
+                    {{ projectType }}
+                    </button>
+                </div>
+                <br>
+            </div>
+
+            <div class="card">
+            <div class="container">
+                <h4><b><nuxt-link :to="`projects/${project.slug}`" v-for="project in filteredSelection">
+                {{ project.name}} <br>
+                </nuxt-link></b></h4> 
+            </div>
+            </div>
+        </div>
 </div>
+
+
+</body>
+
 </template>
 
 <script setup>
+import "@/assets/css/style.css";
 
 const {find} = useStrapi()
 const projects = ref()
@@ -33,7 +58,7 @@ onMounted( async() => {
     projects.value = await find('projets', {populate :'deep'})
     types.value = new Set(projects.value.data.map(project => project.type))
 })
-    
+
 </script>
 
 <style scoped>
